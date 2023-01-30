110f43800eae0438d7d0b33ecb9463407feac5e1
Juhi Jadhav <54070727+Juhij2@users.noreply.github.com>
2023-01-29 19:47:08 -0500

    File uploads

---
+++ app/src/main/java/edu/uncc/inclass10/PostsFragment.java
@@ -0,0 +1,233 @@
+package edu.uncc.inclass10;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import edu.uncc.inclass10.databinding.FragmentPostsBinding;
+import edu.uncc.inclass10.databinding.PostRowItemBinding;
+import edu.uncc.inclass10.models.Post;
+
+public class PostsFragment extends Fragment {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static PostsFragment newInstance(String param1, String param2) {
+        PostsFragment fragment = new PostsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    FragmentPostsBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentPostsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.createPost();
+            }
+        });
+
+        binding.buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.logout();
+            }
+        });
+
+        binding.recyclerViewPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        postsAdapter = new PostsAdapter();
+        binding.recyclerViewPosts.setAdapter(postsAdapter);
+
+        getActivity().setTitle(R.string.posts_label);
+
+        binding.textViewTitle.setText("Welcome" + ""+ mAuth.getCurrentUser().getDisplayName());
+
+        db.collection("posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                mPosts.clear();
+
+                for(QueryDocumentSnapshot document:value){
+
+                    Post post = new Post();
+                    post.created_at = (String) document.getData().get("created_at");
+                    post.post_id = (String) document.getData().get("post_id");
+                    post.post_text = (String) document.getData().get("post_text");
+                    post.created_by_uid = (String) document.getData().get("created_by_uid");
+                    post.created_by_name = (String) document.getData().get("created_by_name");
+                    mPosts.add(post);
+                }
+                postsAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+    }
+
+    PostsAdapter postsAdapter;
+    ArrayList<Post> mPosts = new ArrayList<>();
+
+    class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHolder> {
+        @NonNull
+        @Override
+        public PostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            PostRowItemBinding binding = PostRowItemBinding.inflate(getLayoutInflater(), parent, false);
+            return new PostsViewHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostsViewHolder holder, int position) {
+            Post post = mPosts.get(position);
+            holder.setupUI(post);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPosts.size();
+        }
+
+        class PostsViewHolder extends RecyclerView.ViewHolder {
+            PostRowItemBinding mBinding;
+            Post mPost;
+            public PostsViewHolder(PostRowItemBinding binding) {
+                super(binding.getRoot());
+                mBinding = binding;
+            }
+
+            public void setupUI(Post post){
+                mPost = post;
+                mBinding.textViewPost.setText(post.getPost_text());
+                mBinding.textViewCreatedBy.setText(post.getCreated_by_name());
+                mBinding.textViewCreatedAt.setText(post.getCreated_at());
+
+                if(mPost != null && mPost.getCreated_by_name() != null && mPost.getCreated_by_name().equals(FirebaseAuth.getInstance().getCurrentUser().getDisplayName())){
+                    mBinding.imageViewDelete.setVisibility(View.VISIBLE);
+                    mBinding.imageViewDelete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            deleteAlert(mPost, "Do you want to delete?");
+
+
+
+                        }
+                    });
+                }else{
+                    mBinding.imageViewDelete.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            private void deleteAlert(Post mPost, String message){
+                AlertDialog dialog = new AlertDialog.Builder(getActivity()).setTitle("Delete")
+                        .setMessage(message).
+                        setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        deletePost(mPost);
+
+                                    }
+                                });
+
+
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+                dialog.show();
+            }
+
+        }
+
+    }
+
+    private void deletePost(Post mPost){
+
+        FirebaseFirestore.getInstance().collection("posts").document(mPost.getPost_id()).delete();
+
+    }
+
+
+
+
+
+    PostsListener mListener;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (PostsListener) context;
+    }
+
+    interface PostsListener{
+        void logout();
+        void createPost();
+    }
+}
